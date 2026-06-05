145ab025f7e39ae760aa966029bbf175fe8f2a96
daxnv <da.nv@web.de>
2026-06-05 08:05:47 +0200

    Add logging to create_journeys.js

--- src/create_journeys.js
+++ src/create_journeys.js
@@ -14,64 +14,69 @@ async function main() {
         FOREIGN KEY (origin) REFERENCES stations(id),
         FOREIGN KEY (destination) REFERENCES stations(id),
         UNIQUE (origin, destination)
-    );
+  `);
 
-    INSERT INTO routes (origin, destination)
+  const inserts = [
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1, stations s2
     WHERE s1.name='Bonn Hbf' AND s2.name='Karlsruhe Hbf'
-    ON CONFLICT DO NOTHING;
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='Karlsruhe Hbf'
-    ON CONFLICT DO NOTHING;
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='Köln Hbf' AND s2.name != 'Bonn Hbf'
-    ON CONFLICT DO NOTHING;
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='München Hbf' AND s2.name NOT IN ('Bonn Hbf','Hamburg Hbf')
-    ON CONFLICT DO NOTHING;
-
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='Hamburg Hbf' AND s2.name NOT IN ('Bonn Hbf','München Hbf')
-    ON CONFLICT DO NOTHING;
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='Berlin Hbf' AND s2.name != 'Bonn Hbf'
-    ON CONFLICT DO NOTHING;
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='Hannover Hbf' AND s2.name != 'Bonn Hbf'
-    ON CONFLICT DO NOTHING;
-
-    INSERT INTO routes (origin, destination)
+    ON CONFLICT DO NOTHING;`,
+    `INSERT INTO routes (origin, destination)
     SELECT s1.id, s2.id
     FROM stations s1
     JOIN stations s2 ON s1.id != s2.id
     WHERE s1.name='Frankfurt(Main)Hbf' AND s2.name != 'Bonn Hbf'
-    ON CONFLICT DO NOTHING;
-  `);
+    ON CONFLICT DO NOTHING;`,
+  ];
+
+  let changes = 0;
+  for (const insert of inserts) {
+    try {
+      const info = db.prepare(insert).run();
+      changes += info.changes;
+    } catch (error) {
+      console.error(`Error executing query\n${insert}:`);
+      console.error(error);
+    }
+  }
+  console.log(`Inserted ${changes} routes into ${process.env.DB_PATH}`);
 }
 
 main().catch((error) => {
